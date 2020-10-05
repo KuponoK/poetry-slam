1957723ea1531c6f31e723ed790a1fd0f3eeabfc
Kupono_Kealiinohomoku <kupono.kealiinohomoku@gmail.com>
2020-10-05 16:14:20 -0700

    got file lines
    Started the main.py code

---
+++ poetry.py
@@ -0,0 +1,10 @@
+# recieved help from Joi Anderson
+
+from random import choice
+
+def get_file_lines(filename):
+    
+    in_file = open(filename, "r")
+    lines = in_file.readlines()
+    in_file.close()
+    return lines 
